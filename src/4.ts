61064c4b7581be5a53d0bae7ab8aeb265b69184a
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-10-06 12:04:17 +0300

    ADD:done by goit-typescript-hw-02

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor(signature: number = Math.random()) {
+    this.signature = signature;
+  }
 
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+  }
+
+  abstract OpenDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`Welcome to hell bro ${person.getKey().getSignature()}you have valid key`);
+    } else {
+      console.log(`Not this time`);
+    }
+  }
+}
+
+class MyHouse extends House {
+  OpenDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('You can come in');
+    } else {
+      console.log('The door is locked');
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(key);
 
 house.comeIn(person);
-
 
 export {};
